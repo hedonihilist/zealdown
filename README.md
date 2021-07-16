b350b9976c19510cd2f62fdf7fe2c5a4778c7f35
Hedonihilist <zenghao_mail@163.com>
2021-07-16 17:08:16 +0800

    添加文档

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 用于手动下载、安装Zeal的docset
 
+由于种种原因，可能无法在zeal中直接安装docset，也无法通过feed安装docset，这个脚本用于手动下载压缩包，并且安装docset。
+
+## 使用方法
+
+TODO
+
 ## TODO
 
 - 根据网络情况，选择可访问的源
@@ -9,3 +15,52 @@
 - 指定目录，一键安装docset
 - ✅输出zeal中的docset列表
 - ✅输出zeal中的docset的下载连接
+
+--
+
+## 一些文档
+
+### docset
+
+一个docset就是一个压缩包，要安装docset，可以：
+1. 通过zeal的docset列表安装
+2. 通过添加feed安装docset
+3. 手动下载docset，并且解压到zeal的docset目录下
+
+优先选择前面两种安装方法，因为这样安装的docset可能得到及时的更新。
+
+### docset来源
+
+docset的来源有：
+- 官方来源
+- 用户贡献
+
+**官方来源**
+
+官方的docset就是在zeal的docset列表中可以直接下载的docset，要获取所有docset的列表以及feed可以访问这个链接：
+```
+https://api.zealdocs.org/v1/docsets
+```
+
+**用户贡献**
+
+用户贡献的docset可以在[这个网站](https://api.zealdocs.org/v1/docsets)
+
+虽说有两个来源，但是最终都是从同一个域名下载，即`kapeli.com`，存放docset的服务器在不同的地区有不同的域名，下面是域名列表：
+
+```
+singapore.kapeli.com
+tokyo.kapeli.com
+london.kapeli.com
+newyork.kapeli.com
+sanfrancisco.kapeli.com
+frankfurt.kapeli.com
+sydney.kapeli.com
+mumbai.kapeli.com
+```
+
+这个脚本最重要的功能就是，找到可以访问的域名，并下载docset。
+
+### 如何手动安装docset
+
+下载docset的压缩包，然后解压到zeal的docset目录下即可。
--- zealdown.py
+++ zealdown.py
@@ -1,28 +1,61 @@
 import argparse
+import requests
 import json
 import os
 import urllib.request
 import urllib
+from urllib.parse import urljoin
 import sys
 
 zealapi_url = 'http://api.zealdocs.org/v1/docsets'
+usercontribution_url = 'https://zealusercontributions.vercel.app/api/docsets'
 zealapi_cache_filename = 'docsets.json'
+viable_source = None
 
-valid_source = [
-        'singapore',
-        'tokyo',
-        'london',
-        'newyork',
-        'sanfrancisco',
-        'frankfurt',
-        'sydney',
-        'mumbai'
-    ]
+source_candidates = [
+    'singapore',
+    'tokyo',
+    'london',
+    'newyork',
+    'sanfrancisco',
+    'frankfurt',
+    'sydney',
+    'mumbai',
+    ''  # fallback
+]
 
+######## 网络访问相关
+
+def is_accessible(url, timeout=5):
+    try:
+        r = requests.get(url, timeout=timeout)
+    except Exception:
+        return False
+    return True
+
+def get_source(city):
+    if len(city) == 0: return 'https://kapeli.com'
+    return 'https://' + city + '.kapeli.com'
+
+def get_viable_source(timeout=5):
+    if viable_source is not None:
+        return get_source(viable_source)
+
+    for city in source_candidates:
+        if is_accessible(get_source(city)):
+            viable_source = city
+            return get_source(city)
+        else:
+            print('{} not accessible'.format(get_source(city)))
+    print('Failed to find accessible source', file=sys.stderr)
+    sys.exit(-1)
+
+def prefixed_url(path):
+    source = get_viable_source()
+    return urljoin(source, path)
 
 def get_download_url(source, docset_name):
     return 'http://{}.kapeli.com/feeds/{}.tgz'.format(source, docset_name)
-
 
 def get_docset_list():
     docsets = None
@@ -50,7 +83,6 @@ def download_and_save(url, filename, show_progress=True):
     def progress(chunk_i, chunk_num, total_size):
         sys.stdout.write('\r{} [{}/{}]'.format(filename, chunk_i, chunk_num))
         #print('%d/%d %d' % (chunk_i, chunk_num, total_size))
-
     urllib.request.urlretrieve(url, filename, progress)
 
 def parse_args():
